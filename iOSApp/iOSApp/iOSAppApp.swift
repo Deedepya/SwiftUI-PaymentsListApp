a404287710d04a8f9db4dbb44982da76118f170b
Deedepya <dedeepyaitech@gmail.com>
2025-08-12 09:19:30 -0500

    Added iOSApp

---
+++ iOSApp/iOSApp/iOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSAppApp.swift
+//  iOSApp
+//
+//  Created by dedeepya reddy salla on 12/08/25.
+//
+
+import SwiftUI
+
+@main
+struct iOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
